e605e8dbb1cbdaefc2317d440dac779445ba44c7
AndreaMattone <mattone.andrea97@gmail.com>
2022-01-12 17:30:23 +0100

    adding gateway

---
+++ simplesurf-gateway/src/main/java/com/example/simplesurfgateway/SimplesurfGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplesurfgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesurfGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesurfGatewayApplication.class, args);
+	}
+
+}
---
+++ simplesurf-gateway/src/test/java/com/example/simplesurfgateway/SimplesurfGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplesurfgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesurfGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
